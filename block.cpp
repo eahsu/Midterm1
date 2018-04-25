841da2d506a9957e5883fbe70b6fbef353e73ceb
eahsu <38053522+eahsu@users.noreply.github.com>
2018-04-25 10:05:16 -0700

    Update block.cpp

--- block.cpp
+++ block.cpp
@@ -6,28 +6,22 @@
 using namespace std;
 
 int main(){
-
 	int rows, cols; //declare rows and cols are integer
 	int checkifrowsisnotzero = 0;
 	while (checkifrowsisnotzero == 0){
 		cout << "Enter number of rows and columns: " << endl;
 		cin >> rows >> cols; //enter rows, then a space, then cols
-
 		if (rows > 0){
 			for (int i = 0; i < rows; i++ ){
 				for (int j = 0; j < cols ; j++ ){
 					cout << "X.";
-
 				}
 				cout << endl;
 			}
-
-		}
-			
+		}	
 		else {
 			checkifrowsisnotzero = 1;
 		}
 	}
-
 	return 0;
 }
